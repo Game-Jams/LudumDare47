f7cd4001f27ac126df7439b06d3557c9663c3b5a
tragacheyskiy <de.stolyarov@gmail.com>
2020-10-03 20:23:34 +0500

    Timer added

---
+++ Assets/Scripts/Services/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering.UI;
+
+namespace Scripts.Services
+{
+    public class Timer : MonoBehaviour
+    {
+        public static Timer Instance { get; private set; }
+
+        private SortedList<float, Action> _triggersWithTime = new SortedList<float, Action>();
+        private float _timer = 0.0f;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            CheckTime();
+        }
+
+        private void CheckTime()
+        {
+            int index = _triggersWithTime.IndexOfKey(_timer);
+            if (index > -1)
+            {
+                _triggersWithTime[index]?.Invoke();
+                _triggersWithTime.RemoveAt(index);
+            }
+        }
+
+        public void RegisterTrigger(float time, Action trigger)
+        {
+            if (trigger != null && !_triggersWithTime.ContainsKey(time))
+            {
+                _triggersWithTime.Add(_timer + time, trigger);
+            }
+        }
+    }
+}
