6d089946c2919f70aeb113c3a0c3f86f9b11a4b2
Viktor <risider@yandex.ru>
2020-10-03 19:21:50 +0300

    PlayerController with navMesh

---
+++ Assets/Scripts/Player/Control/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+#pragma warning disable CS0649
+namespace Player.Control
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    internal sealed class PlayerController : MonoBehaviour
+    {
+        private Transform _transform;
+        private NavMeshAgent _agent;
+
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 direction = new Vector3(-horizontal, 0f, -vertical).normalized;
+
+            _agent.SetDestination(_transform.position + direction);
+        }
+    }
+}
