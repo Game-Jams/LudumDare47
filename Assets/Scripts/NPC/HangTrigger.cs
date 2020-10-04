44810b207ac9b35b7562db8ae1fad74d14f8c5cc
Andrew <Anrewqa@yandex.ru>
2020-10-04 23:31:14 +0300

    Intro base + icons

---
+++ Assets/Scripts/NPC/HangTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HangTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    public void Hang()
+    {
+        _animator.SetTrigger(Animator.StringToHash("Hang"));
+    }
+}
