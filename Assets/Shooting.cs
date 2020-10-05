8dfe4ed68d7d3b1eb0576b3becf1a2f5ab5fda8f
Andrew <Anrewqa@yandex.ru>
2020-10-05 20:39:32 +0300

    Shooting

---
+++ Assets/Shooting.cs
@@ -0,0 +1,31 @@
+﻿using Player.Control;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private PlayerController _controller;
+    
+    [SerializeField] private Animator _targetAnimator;
+    
+    [SerializeField] private ParticleSystem[] _systems;
+    
+    public void ShootTarget()
+    {
+        _targetAnimator.SetTrigger("Shooted");
+        
+        foreach (ParticleSystem system in _systems)
+        {
+            system.Play();
+        }
+    }
+
+    public void StopMoving()
+    {
+        _controller.enabled = false;
+    }
+    
+    public void ContinueMoving()
+    {
+        _controller.enabled = true; 
+    }
+}
