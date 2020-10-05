8529ed945eafa61f39d84715b4af87608d02a8f3
Viktor <risider@yandex.ru>
2020-10-05 18:39:22 +0300

    Beginning of game

---
+++ Assets/Scripts/GameActions/Controleers/WagonController.cs
@@ -0,0 +1,31 @@
+﻿using Observable;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GameActions
+{
+    [RequireComponent(typeof(PlayableDirector))]
+    public class WagonController : MonoBehaviour, IGameActionInvokedListener
+    {
+        private PlayableDirector _director;
+
+        private void Awake()
+        {
+            _director = GetComponent<PlayableDirector>();
+
+            this.Subscribe<IGameActionInvokedListener, GameActionParams>();
+        }
+
+        private void Destroy()
+        {
+            this.Unsubscribe<IGameActionInvokedListener, GameActionParams>();
+        }
+
+        void IObserver<IGameActionInvokedListener, GameActionParams>.Completed(GameActionParams parameters)
+        {
+            _director.Play();
+
+            this.Unsubscribe<IGameActionInvokedListener, GameActionParams>();
+        }
+    }
+}
